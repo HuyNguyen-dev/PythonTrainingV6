ca9aac7734ede1940bdc33b63ebace87e5544d1e
nvhuy5 <nvhuy5@tma.com.vn>
2023-03-08 15:14:35 +0700

    update 08032023

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,19 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from urllib.parse import quote_plus
-# from flask_cors import CORS
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote_plus
+from flask_cors import CORS
 
 
-# app = Flask(__name__)
-# app.config.from_object('config')
-# CORS(app)
+app = Flask(__name__)
+app.config.from_object('config')
+CORS(app)
 
-# db = SQLAlchemy()
-# db.init_app(app)
+db = SQLAlchemy()
+db.init_app(app)
 
-# from app.models import models
-# from app.routes import api_routes
-# from app.routes import site_routes
+from app.models import models
+from app.routes import api_routes
+from app.routes import site_routes
 
-# app.register_blueprint(api_routes.api_routes)
-# app.register_blueprint(site_routes.site_routes)
+app.register_blueprint(api_routes.api_routes)
+app.register_blueprint(site_routes.site_routes)
